0b387a5fd013d502c86cf5a3bfaa92ae113d999c
Your Name <you@example.com>
2021-12-03 14:50:20 +0700

    CRUD with Item

---
+++ WebApi-dotnet-6/Controllers/ItemController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_dotnet_6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private static List<Item> items = new List<Item>
+            {
+                new Item { Id = 1,
+                    Name ="Item1",
+                    Description="lorem",
+                    Title="Doe",
+                    Url="asdasdas"
+                },
+                new Item { Id = 2,
+                    Name ="Item2",
+                    Description="333333333333333",
+                    Title="44444444444444444",
+                    Url="55555555555555"
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> Get()
+        {
+
+            return Ok(items);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Item>> Get(int id)
+        {
+            var item = items.Find(i => i.Id == id);
+            if (item == null)
+                return BadRequest("Not Found!!!");
+            return Ok(item);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<Item>>> Add(Item item)
+        {
+            items.Add(item);
+
+            return Ok(items);
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<Item>>> Update(Item request)
+        {
+            var item = items.Find(i => i.Id == request.Id);
+            if (item == null)
+                return BadRequest("Not Found!!!");
+            item.Name = request.Name;
+            item.Description = request.Description;
+            item.Title = request.Title;
+            item.Url = request.Url;
+            return Ok(items);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Item>>> Delete(int id)
+        {
+            var item = items.Find(i => i.Id == id);
+            if (item == null)
+                return BadRequest("Not Found!!!");
+            items.Remove(item);
+            return Ok(item);
+        }
+    }
+}
---
+++ WebApi-dotnet-6/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApi_dotnet_6
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;    
+        public string Description { get; set; } =   string.Empty ;
+        public string Title { get; set; }   = string .Empty ;
+        public string Url { get; set; } = string.Empty;
+        
+    }
+}
